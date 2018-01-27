3e89826ec34fb160ede2d573056a001783853d4d
LuisMi1245 <35621197+LuisMi1245@users.noreply.github.com>
2018-01-26 23:14:27 -0500

    Optimización
    Cambios visuales

---
+++ IOT/www/fisicalab/funcion.php
@@ -0,0 +1,28 @@
+<?php
+function redondear($valor_a_truncar, $decimales){
+switch ($decimales) {
+case 1:
+$float_truncado = round($valor_a_truncar * 10)/10;
+return $float_truncado;
+break;
+case 2:
+$float_truncado = round($valor_a_truncar * 100)/100;
+return $float_truncado;
+break;
+case 3:
+$float_truncado = round($valor_a_truncar * 1000)/1000;
+return $float_truncado;
+break;
+case 4:
+$float_truncado = round($valor_a_truncar * 10000)/10000;
+return $float_truncado;
+break;
+case 5:
+$float_truncado = round($valor_a_truncar * 100000)/100000;
+return $float_truncado;
+break;
+default:
+echo 'NO ES POSIBLE TRUNCAR';     
+break;
+}}
+?>
--- IOT/www/physicsdev.php
+++ IOT/www/physicsdev.php
@@ -47,53 +47,41 @@
         <li><a class="subheader">Síganos</a></li>
         <li><a href="#!"><i class="material-icons">cloud</i>Acerca de nosotros</a></li>
         <li><a href="#!"><i class="material-icons">cloud</i>Proyecto PhysicslAB</a></li>
-        <li><a href="#!"><i class="material-icons">cloud</i>Posibles participantes</a></li>
+        <li><a href="#!"><i class="material-icons">cloud</i>Participantes</a></li>
       </ul>
       <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
   </nav>
+  <br>
 
-  <!-- Physics lab proceso -->
-  <table class="highlight centered responsive-table flow-text white">
-    <thead class="z-depth-2">
-      <tr>
-        <th style="font-size: 18px">Datos</th>
-        <th style="font-size: 18px">Magnitud</th>
-        <th style="font-size: 18px">Sensor</th>
-        <th style="font-size: 18px">Fecha</th>
-      </tr>
-    </thead>
-    <tbody>
 
-      <?php
+
+  <div class="container">
+
+
+    <ul class="collapsible" data-collapsible="accordion">
+      <li>
+        <div class="collapsible-header"><i class="material-icons">filter_drama</i>Velocidades:</div>
+        <div class="collapsible-body">
+
+
+
+          <!-- Physics lab proceso -->
+          <table class="highlight centered responsive-table flow-text white">
+
+            <thead class="z-depth-2">
+              <tr>
+                <th style="font-size: 18px">Datos</th>
+                <th style="font-size: 18px">Magnitud</th>
+                <th style="font-size: 18px">Sensor</th>
+                <th style="font-size: 18px">Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <?php
 require 'fisicalab/conexion.php'; //Agregamos la conexión|
-
-function redondear($valor_a_truncar, $decimales){
-switch ($decimales) {
-case 1:
-$float_truncado = round($valor_a_truncar * 10)/10;
-return $float_truncado;
-break;
-case 2:
-$float_truncado = round($valor_a_truncar * 100)/100;
-return $float_truncado;
-break;
-case 3:
-$float_truncado = round($valor_a_truncar * 1000)/1000;
-return $float_truncado;
-break;
-case 4:
-$float_truncado = round($valor_a_truncar * 10000)/10000;
-return $float_truncado;
-break;
-case 5:
-$float_truncado = round($valor_a_truncar * 100000)/100000;
-return $float_truncado;
-break;
-default:
-echo 'NO ES POSIBLE TRUNCAR';     
-break;
-}}
+require 'fisicalab/funcion.php';
 
 $res = mysqli_query($conexion,"SELECT*FROM datos  WHERE Sensor='Infrar_1_2'") or die(mysqli_error()); 
 
@@ -145,112 +133,136 @@ echo '<td>'. $time = date("H:i a - j M").'</td>';
 echo '</tr>';
 }
 
-include("fisicalab/cerrar_conexion.php");?>
-    </tbody>
-  </table>
+include("fisicalab/cerrar_conexion.php");
+?>
+            </tbody>
+          </table>
+        </div>
+      </li>
+
+      <li>
+        <div class="collapsible-header"><i class="material-icons">place</i>Aceleración:</div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+
+    </ul>
+
+  </div>
 
 
   <div class="green">
 
-    <div class="row">
-      <div class="z-depth-2">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">Otras magnitudes</span>
-            <p class="flow-text">Distancia: Recuerda que la distancia, en este caso, es una magnitud figurativamente constante, ya que estas se encuentran predefinidas y no se modifican, es decir, son valores fijos entre sensores.</p>
-            <br>
-            <span class="card-title">Distancias:</span>
-            <ul class="flow-text">
-              <li>Dist_1 (Sensor 1 y 2) = 0.30 cm</li>
-              <li>Dist_2 (Sensor 2 y 3) = 0.43 cm</li>
-              <li>Dist_3 (Sensor 3 y 4) = 0.26 cm</li>
-              <li>Dist_4 (Sensor 4 y 5) = 0.25 cm</li>
-            </ul>
-          </div>
-          <div class="card-action">
-            <a href="physicsdev.php">Generar magnitudes derivadas (Velocidad y aceleración)</a>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="z-depth-2">
+          <div class="card blue-grey darken-1 card-panel hoverable">
+            <div class="card-content white-text">
+              <span class="card-title"><hr>Otras magnitudes<hr></span>
+              <p class="flow-text">Distancia: Recuerda que la distancia, en este caso, es una magnitud figurativamente constante, ya que estas se encuentran predefinidas y no se modifican, es decir, son valores fijos entre sensores.</p>
+              <br>
+              <span class="card-title">Distancias:</span>
+              <ul class="flow-text">
+                <li>Dist_1 (Sensor 1 y 2) = 0.30 cm</li>
+                <li>Dist_2 (Sensor 2 y 3) = 0.43 cm</li>
+                <li>Dist_3 (Sensor 3 y 4) = 0.26 cm</li>
+                <li>Dist_4 (Sensor 4 y 5) = 0.25 cm</li>
+              </ul>
+            </div>
+            <div class="card-action">
+              <a href="physicsdev.php">Generar magnitudes derivadas (Velocidad y aceleración)</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-
-    <div class="row">
-      <div class="z-depth-2">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title"> 
+    <div class="container">
+      <div class="row">
+        <div class="z-depth-2">
+          <div class="card blue-grey darken-1 card-panel hoverable">
+            <div class="card-content white-text">
+              <span class="card-title"> 
 <hr>Consejos y actividades</span>
-            <hr>
-            <br>
-            <p class="flow-text">Consejos:
-              <ol class="flow-text">
-                <li> Recuerda que las magnitudes derivadas, son aquellas que se definen en función de las fundamentales. Como por ejemplo: Velocidad, aceleración, fuerza, área, entre otras.</li><br>
-                <li>En cambio, las magnitudes fundamentales son las que se definen por sí solas, es decir, independientemente, como la longitud, la masa, el tiempo, la cantidad de materia, etc...</li><br>
-                <li>Cabe recalcar, así mismo, que en la fisica encontramos sistemas que definen la unidad de medida para comparar con ella otras cantidades de la misma especie. Por lo tanto, cualquier sistema, se caracteriza por el conjunto de unidades utilizados
-                  en ella.<br> En fisíca se emplean disferentes sistemas, pero en este caso, haremos uso del sistema C.G.S (centímetro, gramo, segundo).
-                  <br>
-                  <br>
-                  <h5>Fundamentales:</h5>
-                  <ul>
-                    <li>Longitud ---> Centímetro (cm)</li>
-                    <li>Masa ------> Gramo (gr)</li>
-                    <li>Tiempo -----> Segundo (s)</li>
-                  </ul>
-                  <br>
-                  <h5>Derivadas:</h5>
-                  <ul>
-                    <li>Velocidad ---> Centímetro sobre segundo (cm/s)</li>
-                    <li>Aceleración -> Centímetro sobre segundo cuadrado (cm/s°2)</li>
-                    <li>Área -------> Centímetro cuadrado (cm°2)</li>
-                    <li>Volumen -----> Centímetro cúbico (cm°3)</li>
-                  </ul>
-                  <br>
-                </li>
-                <li><a href="#">Actividades y ejercicios propuestos</a></li>
-              </ol>
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="#">Generar magnitudes derivadas (Velocidad y aceleración)</a>
+              <hr>
+              <br>
+              <p class="flow-text">Consejos:
+                <ol class="flow-text">
+                  <li> Recuerda que las magnitudes derivadas, son aquellas que se definen en función de las fundamentales. Como por ejemplo: Velocidad, aceleración, fuerza, área, entre otras.</li><br>
+                  <li>En cambio, las magnitudes fundamentales son las que se definen por sí solas, es decir, independientemente, como la longitud, la masa, el tiempo, la cantidad de materia, etc...</li><br>
+                  <li>Cabe recalcar, así mismo, que en la fisica encontramos sistemas que definen la unidad de medida para comparar con ella otras cantidades de la misma especie. Por lo tanto, cualquier sistema, se caracteriza por el conjunto de unidades
+                    utilizados en ella.<br> En fisíca se emplean disferentes sistemas, pero en este caso, haremos uso del sistema C.G.S (centímetro, gramo, segundo).
+                    <br>
+                    <br>
+                    <h5>Fundamentales:</h5>
+                    <ul>
+                      <li>Longitud ---> Centímetro (cm)</li>
+                      <li>Masa ------> Gramo (gr)</li>
+                      <li>Tiempo -----> Segundo (s)</li>
+                    </ul>
+                    <br>
+                    <h5>Derivadas:</h5>
+                    <ul>
+                      <li>Velocidad ---> Centímetro sobre segundo (cm/s)</li>
+                      <li>Aceleración -> Centímetro sobre segundo cuadrado (cm/s°2)</li>
+                      <li>Área -------> Centímetro cuadrado (cm°2)</li>
+                      <li>Volumen -----> Centímetro cúbico (cm°3)</li>
+                    </ul>
+                    <br>
+                  </li>
+                  <li><a href="#">Actividades y ejercicios propuestos</a></li>
+                </ol>
+              </p>
+            </div>
+            <div class="card-action">
+              <a href="#">Generar magnitudes derivadas (Velocidad y aceleración)</a>
+            </div>
           </div>
         </div>
       </div>
+
+
+
+
+      <footer class="page-footer green">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Laboratorio de física.<br>(Creative Commons)</h5>
+              <p class="grey-text text-lighten-4">Derechos de autor: © VicmanMakers 2018.</p>
+              <p>Cualquier contenido asociado a la plataforma PhysicslAB, se atribuyen respectivamente los derechos de autor correspondientes a los desarrolladores del contenido y plataforma.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Instragram</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+            © 2018 Copyright VicmanMakers
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+      </footer>
+
+
     </div>
-   <footer class="page-footer green">
-            <div class="container">
-              <div class="row">
-                <div class="col l6 s12">
-                  <h5 class="white-text">Laboratorio de física.<br>(Creative Commons)</h5>
-                  <p class="grey-text text-lighten-4">Derechos de autor: © VicmanMakers 2018.</p>
-                  <p>Cualquier contenido asociado a la plataforma PhysicslAB, se atribuyen respectivamente los derechos de autor correspondientes a los desarrolladores del contenido y plataforma.</p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                  <h5 class="white-text">Links</h5>
-                  <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Instragram</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="footer-copyright">
-              <div class="container">
-                © 2018 Copyright VicmanMakers
-                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-              </div>
-            </div>
-          </footer>
-
-
-  </div>
-  <script src="js/jquery.js"></script>
-  <script src="js/materialize.min.js"></script>
-  <script>
-    $(".button-collapse").sideNav();
-  </script>
+    <script src="js/jquery.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script>
+      $(".button-collapse").sideNav();
+    </script>
+    <script>
+      $(document).ready(function() {
+        $('.collapsible').collapsible();
+      });
+    </script>
 </body>
 
 </html>
