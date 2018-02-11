b329b70857b5abe8037cfa600bfdb33f33f73584
LuisMi1245 <35621197+LuisMi1245@users.noreply.github.com>
2018-02-11 15:33:37 -0500

    v5.1 Final Version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/Github/physicslab/IOT/www/');
+	header('Location: '.$uri.'/physicslab/IOT/www/');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
